b0f1292b8d83384917d3856f25a79d5eaa90c471
Annisa Dian Amarta <anisadiana525@gmail.com>
2020-05-18 10:27:48 +0700

    First commit

---
+++ app/src/main/java/com/example/musicplayer/MusicController.java
@@ -0,0 +1,17 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
